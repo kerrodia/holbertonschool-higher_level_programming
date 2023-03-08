faaba978a9b9978ec4b5ef2ef59dff42f3f529ca
kerrodia <5658@holbertonstudents.com>
2023-03-08 14:43:59 -0800

    task 11 and 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,31 +1,20 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    num_arr = []
-    total = 0
-    if roman_string is None or isinstance(roman_string, str) is False:
-        return 0
-
-    for i in range(0, len(roman_string)):
-        if roman_string[i] is "I":
-            num_arr.append(1)
-        if roman_string[i] is "V":
-            num_arr.append(5)
-        if roman_string[i] is "X":
-            num_arr.append(10)
-        if roman_string[i] is "L":
-            num_arr.append(50)
-        if roman_string[i] is "C":
-            num_arr.append(100)
-        if roman_string[i] is "D":
-            num_arr.append(500)
-        if roman_string[i] is "M":
-            num_arr.append(1000)
-
-    for i in range(0, len(num_arr)):
-        if i < len(num_arr) - 1:
-            if num_arr[i] < num_arr[i + 1]:
-                total -= num_arr[i]
-            else:
-                total += num_arr[i]
-    total += num_arr[i]
-    return total
+    if not isinstance(roman_string, str) or roman_string is None:
+        return (0)
+    d = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    suma = 0
+    sign = 1
+    lista = []
+    for x in roman_string[::-1]:
+        if x in d:
+            lista.append(d[x])
+    for index, i in enumerate(lista[:-1]):
+        if i <= lista[index + 1]:
+            suma += i * sign
+            sign = 1
+        else:
+            suma += i * sign
+            sign *= -1
+    suma += lista[-1] * sign
+    return (suma)
